9bcf6e54a572ad8b97968adbe884bf40c2ced4d4
Mhone Bogonko <mbogonko@luc.edu>
2023-12-09 05:53:22 +0000

    first commit

--- main.sh
+++ main.sh
@@ -7,15 +7,15 @@ cat data.txt > read.txt
 pwd > pwd.txt
 ls > ls.txt
 cp data.txt copy.txt
-alias d = 'date'
+alias d='date'
 d > date.txt
 wc -w data.txt > textcount.txt
 ps | head -n 5 > process.txt
 ifconfig | head -n 5 > netstat.txt
 mount | head -n 5 > mount.txt
-mkdir permissions.txt
+touch permissions.txt
 chmod 777 permissions.txt
-TESTENV1 = "test"
+TESTENV1="test"
 grep -E '[a-zA-Z]{3,}' data.txt > regex.txt
 cd
 cd extra-credit
